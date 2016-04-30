cd8389774de7864ea6b9e69496d455f2f9f6634a
WangTimo <82086740@qq.com>
2016-04-30 18:38:19 +0800

    chapter9  CSS DOM

---
+++ chapter9/scripts/奇偶行样式.js
@@ -0,0 +1,39 @@
+/*function stripeTables(){
+	if (!document.getElementsByTagName) return false;
+	var tables = document.getElementsByTagName('table');
+	var odd,rows;
+	for (var i=0; i<tables.length; i++){
+		odd = false;
+		rows = tables[i].getElementsByTagName('tr');
+		for (var j=0; j<rows.length; j++){
+			if (odd == true){
+				rows[j].style.backgroundColor = '#fdc';     //对比
+				odd = false;
+			}else{
+				odd = true;
+			}
+		}
+	}
+}
+addLoadEvent(stripeTables);
+*/
+
+//改进版本，通过调用addclass实现同样效果
+function stripeTables(){
+	if (!document.getElementsByTagName) return false;
+	var tables = document.getElementsByTagName('table');
+	var odd,rows;
+	for (var i=0; i<tables.length; i++){
+		odd = false;
+		rows = tables[i].getElementsByTagName('tr');
+		for (var j=0; j<rows.length; j++){
+			if (odd == true){
+				addClass (rows[j],"odd");      
+				odd = false;
+			}else{
+				odd = true;
+			}
+		}
+	}
+}
+addLoadEvent(stripeTables);
