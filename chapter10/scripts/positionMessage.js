e69acce03909ed5074c6312795870b5cb1e6d09c
WangTimo <82086740@qq.com>
2016-05-04 15:22:41 +0800

    chapter10

--- chapter10/scripts/positionMessage.js
+++ chapter10/scripts/positionMessage.js
@@ -3,8 +3,14 @@ function positionMessage() {
 	if (!document.getElementById('message')) return false;
 	var elem = document.getElementById('message');
 	elem.style.position = 'absolute';
-	elem.style.left = '50px';
+	elem.style.left = '500px';
 	elem.style.top = '100px';
-	moveElement("message",200,200,20);
+	moveElement("message",1024,35,20);
+	if (!document.getElementById('message2')) return false;
+	var elem = document.getElementById('message2');
+	elem.style.position = 'absolute';
+	elem.style.left = '500px';
+	elem.style.top = '50px';
+	moveElement('message2',1024,135,20);
 }
 addLoadEvent(positionMessage);	
